e9fb32d1e87e2423eb52c4d74687aefcec5fab68
Kanata-Sora <kanatayuo@gmail.com>
2025-11-08 21:07:11 +0900

    Update torch_note.py
    print(t4)を実行することでGitが動いていることを確認。VScodeで実行しないとgithubに反映されない模様

--- torch_note.py
+++ torch_note.py
@@ -26,3 +26,27 @@ print(t.shape, '-->', t_tr.shape)
 t = torch.zeros(30)
 t_reshape = t.reshape(5, 6)
 print(t_reshape.shape)
+
+#----------------------------------------
+
+torch.manual_seed(1)
+#乱数を固定
+t1 = 2 * torch.rand(5, 2) -1
+t2 = torch.normal(mean=0, std=1, size=(5, 2))
+
+t3 = torch.multiply(t1, t2)
+#二つの行列の積を取っているのではなく、要素ごとに掛け算を行っている
+print(t3)
+
+t4 = torch.mean(t1, axis=0)
+#axis=0は行方向（縦）
+print(t4)
+
+t5 = torch.matmul(t1, torch.transpose(t2, 0, 1))
+print(t5)
+
+norm_t1 = torch.linalg.norm(t1, ord=2, dim=1)
+#ord=2: ノルムの種類：L2ノルム（二乗和の平方根）**を指定
+#dim=1: 列方向（横）
+print(norm_t1)
+print(t4)
