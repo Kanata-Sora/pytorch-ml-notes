f6b3e5f831f749bd4fc7824b591b467b84026127
Kanata-Sora <kanatayuo@gmail.com>
2025-12-10 23:01:47 +0900

    Update torch_note.py

--- torch_note.py
+++ torch_note.py
@@ -150,3 +150,41 @@ for i, file in enumerate(file_list):
 
 plt.tight_layout()
 plt.show()
+#----------------------------------
+#p362 
+
+import torchvision.transforms as transforms
+img_height, img_width = 80, 120 
+transform = transforms.Compose([transforms.ToTensor(),
+                                transforms.Resize((img_height, img_width)),])
+
+labels = [1 if 'dog' in os.path.basename(file) else 0 for file in file_list]
+
+class ImageDataset(Dataset):
+    
+    def __init__(self, file_list, labels, transform=None):
+        self.file_list =file_list
+        self.labels = labels
+        self.transform = transform
+
+    def __getitem__(self, index):
+        img = Image.open(self.file_list[index])
+        if self.transform is not None:
+            img = self.transform(img)
+        label = self.labels[index]
+        return img, label
+    
+    def __len__(self):
+        return len(self.labels)
+    
+image_dataset = ImageDataset(file_list, labels, transform)
+
+fig = plt.figure(figsize=(10, 6))
+for i, example in enumerate(image_dataset):
+    ax = fig.add_subplot(2, 3, i+1)
+    ax.set_xticks([]); ax.set_yticks([])
+    ax.imshow(example[0].numpy().transpose((1,2,0)))
+    ax.set_title(f'{example[1]}', size=15)
+
+plt.tight_layout()
+plt.show()
